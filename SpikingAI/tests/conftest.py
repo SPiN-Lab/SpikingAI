bba70315a2d7a8f063e3d9aec4a86f9c754dc370
vinferrer <vferrer02@proton.me>
2023-11-24 15:51:29 +0100

    add testing configuration file

---
+++ SpikingAI/tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+import ssl
+from urllib.request import urlretrieve
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--skipintegration", action="store_true", default=False, help="Skip integration tests."
+    )
+
+
+@pytest.fixture
+def skip_integration(request):
+    return request.config.getoption("--skipintegration")
